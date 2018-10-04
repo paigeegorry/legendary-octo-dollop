0146a9d50d2f4a2084faccbbf242c538c0f208ed
Paige Gorry <paigeegorry@gmail.com>
2018-10-03 20:42:55 -0700

    demo prompt

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-var x = 12;
-
-//(consition that if true, code block runs)
-if (x % 2 === 0){
-    console.log(x, 'is even!');
-}
 
 //(innit; condition; post-loop)
 
-for(var i = 0; i <= 100; i++){
+var countTo = prompt('how high should we count even numbers?');
+
+var sure = confirm('are you sure you want to count to '+ countTo'?');
+
+if (sure) {
+for(var i = 0; i < countTo; i++){
    var remainder (i %2);
     if (remainder === 0) {
        console.log('the number is', i); 
     }
 }
-
+}
 console.log('all done');
